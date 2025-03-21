4e50a836e334eae37ee6e03730f881d705b6e960
Dixita-busa <dixita.busa.2203@gmail.com>
2025-03-21 19:34:48 +0530

    Test-2 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-2';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('previewBox') previewBox!: ElementRef<HTMLDivElement>;
+
+  primaryColor: string = '#fff';
+  secondaryColor: string = '#000';
+
+  ngAfterViewInit(): void {
+    this.applyThemeColors();
+  }
+
+  onPrimaryColorChange(event: Event): void {
+    this.primaryColor = (event.target as HTMLInputElement).value;
+    this.applyThemeColors();
+  }
+
+  onSecondaryColorChange(event: Event): void {
+    this.secondaryColor = (event.target as HTMLInputElement).value;
+    this.applyThemeColors();
+  }
+
+  private applyThemeColors(): void {
+    if (this.previewBox && this.previewBox.nativeElement) {
+      const element = this.previewBox.nativeElement;
+      element.style.backgroundColor = this.primaryColor;
+      element.style.color = this.secondaryColor;
+    }
+  }
 }
