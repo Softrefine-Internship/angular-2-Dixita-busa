5d153ba048008d9e02a406114f2162dafd5f585f
Dixita-busa <dixita.busa.2203@gmail.com>
2025-03-21 19:34:48 +0530

    Test-2 Complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,55 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [FormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'angular-2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-2');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should have color picker elements', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-2 app is running!');
+    expect(compiled.querySelector('h2')?.textContent).toContain(
+      'Theme Customizer'
+    );
+    expect(compiled.querySelector('#primaryColor')).toBeTruthy();
+    expect(compiled.querySelector('#secondaryColor')).toBeTruthy();
+  });
+
+  it('should update colors when input values change', () => {
+    component.ngAfterViewInit();
+    fixture.detectChanges();
+
+    const primaryColorInput = fixture.nativeElement.querySelector(
+      '#primaryColor'
+    ) as HTMLInputElement;
+    const secondaryColorInput = fixture.nativeElement.querySelector(
+      '#secondaryColor'
+    ) as HTMLInputElement;
+
+    primaryColorInput.value = '#fff';
+    primaryColorInput.dispatchEvent(new Event('input'));
+
+    secondaryColorInput.value = '#000';
+    secondaryColorInput.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(component.primaryColor).toBe('#fff');
+    expect(component.secondaryColor).toBe('#000');
   });
 });
